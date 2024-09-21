70c9850c7d427e1733552f72d96975b10f0107db
cyuxuan <cyuxuan@163.com>
2024-09-21 18:04:17 +0800

    yapi及mongo安装提交

--- bee-code-test-field/auto-project/docker/shell/docker-yapi-install.sh
+++ bee-code-test-field/auto-project/docker/shell/docker-yapi-install.sh
@@ -9,19 +9,19 @@ docker pull mongo:7.0.3
 docker run -dit --name mongodb --restart always \
   --net=yapi -p 27017:27017 -v /data/docker/mongo:/data/db \
   -e MONGO_INITDB_DATABASE=yapi \
-  -e MONGO_INITDB_ROOT_USERNAME= \
-  -e MONGO_INITDB_ROOT_PASSWORD= \
+  -e MONGO_INITDB_ROOT_USERNAME=beenest \
+  -e MONGO_INITDB_ROOT_PASSWORD=Beenest@666??? \
   mongo:7.0.3
 
 #进入mongo客户端
 docker exec -it mongodb mongosh admin
 
 #初始化数据库,依次执行下面的命令
-db.auth("", "")
+db.auth("beenest", "Beenest@666???")
 #创建yapi数据库
 use yapi;
 # 创建给yapi使用的账号和密码，授予可操作的权限
-db.createUser({user: '', pwd: '', roles: [{ role: "dbAdmin", db: "yapi" },{ role: "readWrite", db: "yapi" }]});
+db.createUser({user: 'beenest', pwd: 'Beenest@666???', roles: [{ role: "dbAdmin", db: "yapi" },{ role: "readWrite", db: "yapi" }]});
 
 # 退出mongo客户端
 exit
@@ -49,14 +49,14 @@ vim /data/docker/yapi/config.json
 # 写入以下内容
 {
    "port": "3000",
-   "adminAccount": "",
+   "adminAccount": "管理员账户",
    "timeout":120000,
    "db": {
      "servername": "mongo",
      "DATABASE": "yapi",
      "port": 27017,
-     "user": "",
-     "pass": "",
+     "user": "数据库用户名称",
+     "pass": "数据库用户密码",
      "authSource": ""
    },
    "mail": {
